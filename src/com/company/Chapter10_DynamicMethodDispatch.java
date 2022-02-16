ccdd222f5f4a249a488b9ace630d48a151b85758
coderaj2001 <biswajit05101963@gmail.com>
2022-02-16 19:24:07 +0530

    We have created dynamic method dispatch

--- src/com/company/Chapter10_DynamicMethodDispatch.java
+++ src/com/company/Chapter10_DynamicMethodDispatch.java
@@ -1,29 +1,42 @@
 package com.company;
 
 
-class One
+class Phone
 {
+
+// This is a greet Method
     public void greet()
     {
         System.out.println("Good Morning");
     }
+//This is a name method;
 
     public void name()
     {
-        System.out.println("My name is java");
+        System.out.println("Turning on the Nokia");
     }
 }
 
-class two extends One
+
+
+//////////////////////////////////////////////////////////////\///////
+
+
+//This is an example of inheritance class.
+
+class SmartPhone extends Phone
 {
+//    This is one method
     public void swagat()
     {
         System.out.println("Apka Swagat hai isme");
     }
 
+// This is another method
+
     public void name()
     {
-        System.out.println("My name is java");
+        System.out.println("Turing on the Smartphone");
     }
 }
 
@@ -32,7 +45,18 @@ class two extends One
 
 public class Chapter10_DynamicMethodDispatch {
     public static void main(String[] args) {
+        // Phone Obj=new Phone();  //Allowed
+        // SmartPhone smobj=new SmartPhone();  //This is also allowed
+
+        Phone obj= new SmartPhone();  //This is also allowed.
+      //  SmartPhone obj2=new Phone();  //THis is not allowed.
+
+        obj.name();  //By running this the second name will be printed.
+
+
 
     }
 
 }
+
+
